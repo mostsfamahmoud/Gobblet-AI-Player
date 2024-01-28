bd361f101999aaf42f28b8f64199851da93fe32f
Abdelrahman-Ali-5 <abdelrahmanelkollaly@gmail.com>
2024-01-28 14:33:10 +0200

    update player.py

--- Players/player.py
+++ Players/player.py
@@ -5,10 +5,12 @@ from GameComponents.pieceCollection import PieceMovement
 
 class Player:
 
+#function to get the name of the player
     @abc.abstractmethod
     def get_name(self) -> str:
         raise NotImplemented
 
+#function to get the player move on the board
     @abc.abstractmethod
     def get_move(self, state: GameState) -> PieceMovement:
         raise NotImplemented
